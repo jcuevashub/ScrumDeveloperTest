9aedc71c94309e355aa6761f12edb80a125823ad
Jackson Cuevas <jackson.cuevas@hotmail.com>
2020-05-16 18:55:17 -0400

    Initial Project

---
+++ MorseCodeTest/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MorseCodeTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestMethod() {
+            Assert.Pass();
+        }
+    }
+}
